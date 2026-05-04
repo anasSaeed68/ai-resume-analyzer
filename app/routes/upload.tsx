7a667bb21e0282702340b30734f410c20ca20bf5
AnasSaeed09 <anassaeed899@gmail.com>
2026-05-04 11:04:20 +0500

    Resume Feedback Page

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -58,7 +58,7 @@ const Upload = () => {
         await kv.set(`resume:${uuid}`, JSON.stringify(data));
         setStatusText("Analysis complete! Redirecting...");
         console.log(data);
-        
+        navigate(`/resume/${uuid}`);
     }
 
 
